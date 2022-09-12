d0287a1f2bc8cfc08c780b220730e60ea8db8f7c
jatan-ops <jatan.shah90@gmail.com>
2022-09-12 19:26:25 +0530

    in react-bootstrap-grid

--- src/demo.js
+++ src/demo.js
@@ -1,25 +1,32 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import { v4 as uuidv4 } from 'uuid';
+
+import GridLayout from "react-grid-layout";
 
 import ReactEditorInstance from "./ReactEditorInstance";
 
-export default function BasicGrid() {
+export default function Demo() {
 
   const cells = [uuidv4(),uuidv4(),uuidv4(),uuidv4()]
 
   return (
-    <Box>
-      <Grid container spacing={2}>     
+    <Container fluid>
+      <Row>
         {
-          cells.map(id => {
-            return <Grid key={id} item md={3}>
-              <ReactEditorInstance holder={id} />
-            </Grid>     
+          cells.map((id,i) => {
+            return <Col md={3}>
+            <ReactEditorInstance holder={id} />
+          </Col> 
           })
         }
-      </Grid>
-    </Box>
+      </Row>
+    </Container>
   );
 }
+
+// tried making this work but did not work - https://www.npmjs.com/package/react-grid-layout
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import Demo from "./demo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<Demo />,rootElement);
